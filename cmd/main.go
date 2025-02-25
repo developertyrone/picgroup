35d86c3eb75c883903ef8c6df04dafc2e73d9c4a
Developer Tyrone <developertyrone@gmail.com>
2025-02-25 22:02:51 +0800

    Add version and build time flags to release workflow and main.go
    - Modify release.yml to include version and build timestamp during compilation
    - Add version flag to main.go to display version and build information
    - Use ldflags to inject version and build time metadata during build process

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,30 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"os"
 
 	"github.com/developertyrone/picgroup/pkg/organizer"
 )
 
+// These variables are set during build using ldflags
+var (
+	Version   = "dev"
+	BuildTime = "unknown"
+)
+
 func main() {
+	// Add version flag
+	versionFlag := flag.Bool("version", false, "Print version information")
+	flag.Parse()
+
+	// Check if version flag was provided
+	if *versionFlag {
+		fmt.Printf("PicGroup version %s (built at %s)\n", Version, BuildTime)
+		os.Exit(0)
+	}
+
 	// Execute sets up flags, instantiates the Organizer and runs it.
 	organizer.Execute()
 
